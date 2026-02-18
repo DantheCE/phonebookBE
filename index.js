6f48a5a79c420da58dd78c8455386e39d831d6a6
DantheCE <dan.doyin@gmail.com>
2026-02-18 14:15:34 +0100

    feat: Refactor PhoneLog model and remove unused database.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PhoneLog = require('./models/database')
+const PhoneLog = require('./models/phoneLog')
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3001
 
