6698530cfcd604020923ce09d7f339b9cbd32306
DantheCE <dan.doyin@gmail.com>
2026-02-18 09:39:01 +0100

    feat: Add phone number validation to phoneLogSchema

--- models/database.js
+++ models/database.js
@@ -19,7 +19,13 @@ const phoneLogSchema = new mongoose.Schema({
     number: {
         type: String, 
         minLength: 3,
-        required: true
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
     },
 })
 
